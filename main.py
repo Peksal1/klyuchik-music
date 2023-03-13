5146c4610f649c70c02141d28503234dbbd37874
Ilja Laurs <iljal@peplink.com>
2023-03-13 15:40:05 +0200

    fix

--- main.py
+++ main.py
@@ -4,17 +4,22 @@ from pytube import YouTube
 import random
 import os
 import discord.opus
+
+# Load the Opus library for audio encoding/decoding
 discord.opus.load_opus('libopus.so.0')
 
-bot_token = os.environ['DISCORD_BOT_TOKEN']
-print('hello')
-# Replace YOUR_TOKEN_HERE with your bot's token
+# Get the bot token from the environment variable
+bot_token = os.environ.get('DISCORD_BOT_TOKEN')
+if not bot_token:
+    print('Error: no bot token specified in DISCORD_BOT_TOKEN environment variable')
+    exit(1)
+
+# Initialize the Discord client
 intents = discord.Intents.default()
 intents.members = True
-
 client = discord.Client(intents=intents)
 
-# List of YouTube video URLs
+# List of YouTube video URLs to play
 video_urls = [
     'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     'https://www.youtube.com/watch?v=2ZIpFytCSVc',
@@ -25,13 +30,26 @@ async def play_song(voice_client):
     # Choose a random video URL from the list
     url = random.choice(video_urls)
 
-    # Download the video
+    # Download the video as an audio stream
     yt = YouTube(url)
     stream = yt.streams.filter(only_audio=True).first()
-    filename = stream.download()
+    if not stream:
+        print(f'Error: could not download audio stream from {url}')
+        return
 
-    # Send the audio to the voice channel
-    voice_client.play(discord.FFmpegPCMAudio(filename))
+    # Save the audio stream to a temporary file
+    filename = f'{yt.video_id}.mp3'
+    stream.download(filename=filename)
+
+    # Send the audio stream to the voice channel
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(filename))
+    voice_client.play(source, after=lambda e: print(f'Finished playing {filename}'))
+    print(f'Playing {filename}')
+
+    # Wait for the song to finish playing before deleting the temporary file
+    while voice_client.is_playing():
+        await asyncio.sleep(1)
+    os.remove(filename)
 
 @client.event
 async def on_ready():
@@ -39,16 +57,21 @@ async def on_ready():
 
     # Find the voice channel with the specified ID
     voice_channel = client.get_channel(712008433443799151)
+    if not voice_channel:
+        print(f'Error: could not find voice channel with ID 712008433443799151')
+        exit(1)
     print(f'Connecting to voice channel {voice_channel.name} ({voice_channel.id})')
 
     # Connect to the voice channel
     voice_client = await voice_channel.connect()
-    print('Connected to voice channel')
+    print(f'Connected to voice channel {voice_channel.name}')
 
-    # Start playing songs
+    # Start playing songs in a loop
     while True:
-        await play_song(voice_client)
-        print('Playing a song')
-        await asyncio.sleep(5) # wait 5 seconds before playing the next song   
+        try:
+            await play_song(voice_client)
+            await asyncio.sleep(5) # wait 5 seconds before playing the next song
+        except Exception as e:
+            print(f'Error: {e}')
 
 client.run(bot_token)
